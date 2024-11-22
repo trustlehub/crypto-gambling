4261065230d2bfa54dd7b0d937464a94bb2f2885
lasan <limahaliyana@gmail.com>
2024-11-22 19:05:53 +0530

    bugfix

--- backend/app.py
+++ backend/app.py
@@ -136,67 +136,62 @@ async def get_events(lay_as_back: bool = None, db: Session = Depends(get_db)):
             # so we want the non matched ones
 
             # If o1 is not bookmaker or o2 is not exchange, then we can't match them
+            if not o1.provider.is_bookmaker or not o2.provider.is_exchange:
+                continue
 
             all_outcomes_matched_with_o1 = [*(mo.outcomes for mo in o1.matched_outcomes)]
-            if lay_as_back:
-                if o2.provider.is_exchange:
-                    # This combination shouldn't be here since we are using bookmakers for both lay and back 
-                    continue
-                if o2 in all_outcomes_matched_with_o1:
-                    # this combination shouldn't be here since same teams are only matched when lay as back is false
-                    lg.info("skipping this combination: same team. Lay as back is set. Should match different outcomes")
-                    continue
-            if not lay_as_back:
-                if o1.provider.is_exchange or not o2.provider.is_exchange:
-                    # This combination shouldn't be here since we are using bookmakers for both lay and back 
-                    lg.info("Skipping this combo. lay as back not set. o1 is exchange or o2 is bookmaker")
-                    continue
-            final_odds.append(
-                # o1 is the bet_team and o2 is the lay team. so o1's provider is 
-                # bookmaker and o2's provider is exchange
-                OddsCleaned(
-                    event=o1.event.name,
-                    time=o1.event.start_time,
-                    bet_team=o1.name,
-                    bookmaker=o1.provider.name,
-                    exchange=o2.provider.name,
-                    odds=o1.market.odds,
-                    lay=o2.market.odds,
-                    avail="10",
-                    home_team=o1.name,
-                    away_team=o2.name,
-                    odds_last_update=datetime.now(tz=timezone.utc).isoformat(),
-                    lay_last_update=datetime.now(tz=timezone.utc).isoformat(),
-                    lay_as_back=o2.provider.is_bookmaker,
-                    rating=rating_calc(
-                        o1.market.odds,
-                        o2.market.odds,
-                        0
-                    ),
-                    maxLay="%.2f" % o2.market.meta[o2.provider.name]['maxStake']
-                           + " " + o2.market.meta[o2.provider.name]['currency'],
-                    back_outcome_id=o1.id,
-                    lay_outcome_id=o2.id,
-                    meta={
-                        o1.provider.name: {
-                            **(o1.meta[
-                                   o1.provider.name] if o1.meta is not None and o1.provider.name in o1.meta.keys() else {}),
-                            **(o1.market.meta[
-                                   o1.provider.name] if o1.market.meta is not None and o1.provider.name in o1.market.meta.keys() else {}),
-                            **(o1.event.meta[
-                                   o1.provider.name] if o1.event.meta is not None and o1.provider.name in o1.event.meta.keys() else {}),
+            if o2 not in all_outcomes_matched_with_o1:
+                lg.info("skipping this combination: same different teams")
+                continue
+            elif o1.provider.name == o2.provider.name:
+                lg.info("skipping this combination: same provider")
+            else:
+                final_odds.append(
+                    # o1 is the bet_team and o2 is the lay team. so o1's provider is 
+                    # bookmaker and o2's provider is exchange
+                    OddsCleaned(
+                        event=o1.event.name,
+                        time=o1.event.start_time,
+                        bet_team=o1.name,
+                        bookmaker=o1.provider.name,
+                        exchange=o2.provider.name,
+                        odds=o1.market.odds,
+                        lay=o2.market.odds,
+                        avail="10",
+                        home_team=o1.name,
+                        away_team=o2.name,
+                        odds_last_update=datetime.now(tz=timezone.utc).isoformat(),
+                        lay_last_update=datetime.now(tz=timezone.utc).isoformat(),
+                        lay_as_back=o2.provider.is_bookmaker,
+                        rating=rating_calc(
+                            o1.market.odds,
+                            o2.market.odds,
+                            0
+                        ),
+                        maxLay="%.2f" % o2.market.meta[o2.provider.name]['maxStake']
+                               + " " + o2.market.meta[o2.provider.name]['currency'],
+                        back_outcome_id=o1.id,
+                        lay_outcome_id=o2.id,
+                        meta={
+                            o1.provider.name: {
+                                **(o1.meta[
+                                       o1.provider.name] if o1.meta is not None and o1.provider.name in o1.meta.keys() else {}),
+                                **(o2.market.meta[
+                                       o2.provider.name] if o2.market.meta is not None and o2.provider.name in o2.market.meta.keys() else {}),
+                                **(o2.event.meta[
+                                       o2.provider.name] if o2.event.meta is not None and o2.provider.name in o2.event.meta.keys() else {}),
+                            },
+                            o2.provider.name: {
+                                **(o2.meta[
+                                       o2.provider.name] if o2.meta is not None and o2.provider.name in o2.meta.keys() else {}),
+                                **(o2.market.meta[
+                                       o2.provider.name] if o2.market.meta is not None and o2.provider.name in o2.market.meta.keys() else {}),
+                                **(o2.event.meta[
+                                       o2.provider.name] if o2.event.meta is not None and o2.provider.name in o2.event.meta.keys() else {}),
+                            }
                         },
-                        o2.provider.name: {
-                            **(o2.meta[
-                                   o2.provider.name] if o2.meta is not None and o2.provider.name in o2.meta.keys() else {}),
-                            **(o2.market.meta[
-                                   o2.provider.name] if o2.market.meta is not None and o2.provider.name in o2.market.meta.keys() else {}),
-                            **(o2.event.meta[
-                                   o2.provider.name] if o2.event.meta is not None and o2.provider.name in o2.event.meta.keys() else {}),
-                        }
-                    },
+                    )
                 )
-            )
-            total += 1
+                total += 1
 
     return sorted(final_odds, key=lambda x: float(x.rating), reverse=True)
